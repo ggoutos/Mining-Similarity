b3dd670602e3b77856fae0c6489a2fd213ce6753
asskickGR <tkd4real@hotmail.com>
2016-03-26 22:42:59 +0200

    update 26/3
    remove whitespace

--- www/js/draw_pie.js
+++ www/js/draw_pie.js
@@ -1,126 +1,160 @@
-function draw_pie(g,c,m,n) {
+function draw_pie(g, c, m, n) {
 
-var width = 300,
-    height = 300,
-    radius = Math.min(width, height) / 2,
-    innerRadius = 0.3 * radius;
+    var width = 300,
+        height = 300,
+        radius = Math.min(width, height) / 2,
+        innerRadius = 0.3 * radius;
 
-var pie = d3.layout.pie()
-    .sort(null)
-    .value(function(d) { return d.width; });
+    var pie = d3.layout.pie()
+        .sort(null)
+        .value(function (d) {
+            return d.width;
+        });
 
-var tip = d3.tip()
-  .attr('class', 'd3-tip')
-  .offset([0, 0])
-  .html(function(d) {
-    return d.data.label + ": <span style='color:orangered'>" + d.data.score + "%</span>";
-  });
+    var tip = d3.tip()
+        .attr('class', 'd3-tip')
+        .offset([0, 0])
+        .html(function (d) {
+            return d.data.label + ": <span style='color:orangered'>" + d.data.score + "%</span>";
+        });
 
-  
-var arc = d3.svg.arc()
-  .innerRadius(innerRadius)
-  .outerRadius(function (d) { 
-    return (radius - innerRadius) * (d.data.score / 100.0) + innerRadius; 
-  });
-  
-  
- var arc_2 = d3.svg.arc()
-  .innerRadius(function (d) { 
-    return (radius - innerRadius) * (d.data.score / 100.0) + innerRadius; 
-  })
-  .outerRadius(radius);
 
-var outlineArc = d3.svg.arc()
+    var arc = d3.svg.arc()
+        .innerRadius(innerRadius)
+        .outerRadius(function (d) {
+            return (radius - innerRadius) * (d.data.score / 100.0) + innerRadius;
+        });
+
+
+    var arc_2 = d3.svg.arc()
+        .innerRadius(function (d) {
+            return (radius - innerRadius) * (d.data.score / 100.0) + innerRadius;
+        })
+        .outerRadius(radius);
+
+    var outlineArc = d3.svg.arc()
         .innerRadius(innerRadius)
         .outerRadius(radius);
 
-var svg = d3.select("#plot").append("svg")
-.attr("width", width)
-    .attr("height", height)
-    .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+    var svg = d3.select("#plot").append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-svg.call(tip);
-
-
+    svg.call(tip);
 
 
-var data = [
-    { label: "Pages", score: g, order: 1, weight: 1, color: "#9E0041", width: 1},
-    { label: "Checkins", score: c, order: 1, weight: 1, color: "#E1514B", width: 1},
-    { label: "Music", score: m, order: 1, weight: 1, color: "#4776B4", width: 1},
-    { label: "Movies", score: n, order: 1, weight: 1, color: "#5E4EA1", width: 1}
+
+
+    var data = [
+        {
+            label: "Pages",
+            score: g,
+            order: 1,
+            weight: 1,
+            color: "#9E0041",
+            width: 1
+        },
+        {
+            label: "Checkins",
+            score: c,
+            order: 1,
+            weight: 1,
+            color: "#E1514B",
+            width: 1
+        },
+        {
+            label: "Music",
+            score: m,
+            order: 1,
+            weight: 1,
+            color: "#4776B4",
+            width: 1
+        },
+        {
+            label: "Movies",
+            score: n,
+            order: 1,
+            weight: 1,
+            color: "#5E4EA1",
+            width: 1
+        }
 ];
 
 
 
-//d3.csv('aster_data.csv', function(error, data) {
+    //d3.csv('aster_data.csv', function(error, data) {
 
-  data.forEach(function(d) {
-    d.id     =  d.id;
-    d.order  = +d.order;
-    d.color  =  d.color;
-    d.weight = +d.weight;
-    d.score  = +d.score;
-    d.width  = +d.weight;
-    d.label  =  d.label;
-  });
-  
-  var path = svg.selectAll(".solidArc")
-      .data(pie(data))
-    .enter().append("path")
-      .attr("fill", function(d) { return d.data.color; })
-      .attr("class", "solidArc")
-      .attr("stroke", "gray")
-      .attr("d", arc)
-      .on('mouseover', tip.show)
-      .on('mouseout', tip.hide);
-	  
-	 var path2 = svg.selectAll(".solidArc2")
-      .data(pie(data))
-    .enter().append("path")
-      .attr("fill", function(d) { return d.data.color; })
-	  .attr("opacity", 0)
-      .attr("class", "solidArc2")
-      .attr("stroke", "gray")
-      .attr("d", arc_2)
-      .on('mouseover', tip.show)
-      .on('mouseout', tip.hide);
+    data.forEach(function (d) {
+        d.id = d.id;
+        d.order = +d.order;
+        d.color = d.color;
+        d.weight = +d.weight;
+        d.score = +d.score;
+        d.width = +d.weight;
+        d.label = d.label;
+    });
 
-  var outerPath = svg.selectAll(".outlineArc")
-      .data(pie(data))
-    .enter().append("path")
-      .attr("fill", "none")
-      .attr("stroke", "gray")
-      .attr("class", "outlineArc")
-      .attr("d", outlineArc);
+    var path = svg.selectAll(".solidArc")
+        .data(pie(data))
+        .enter().append("path")
+        .attr("fill", function (d) {
+            return d.data.color;
+        })
+        .attr("class", "solidArc")
+        .attr("stroke", "gray")
+        .attr("d", arc)
+        .on('mouseover', tip.show)
+        .on('mouseout', tip.hide);
 
+    var path2 = svg.selectAll(".solidArc2")
+        .data(pie(data))
+        .enter().append("path")
+        .attr("fill", function (d) {
+            return d.data.color;
+        })
+        .attr("opacity", 0)
+        .attr("class", "solidArc2")
+        .attr("stroke", "gray")
+        .attr("d", arc_2)
+        .on('mouseover', tip.show)
+        .on('mouseout', tip.hide);
 
-  // calculate the weighted mean score
-var score=0;
-for(i=0; i<data.length; i++) {
-score += data[i].score;
-}
-score=(score/4).toPrecision(4);
+    var outerPath = svg.selectAll(".outlineArc")
+        .data(pie(data))
+        .enter().append("path")
+        .attr("fill", "none")
+        .attr("stroke", "gray")
+        .attr("class", "outlineArc")
+        .attr("d", outlineArc);
 
 
-  svg.append("svg:text")
-    .attr("class", "aster-score")
-    .attr("dy", ".35em")
-    .attr("text-anchor", "middle") // text-align: right
-  .attr("fill", "#FFF")
-    .text(score+'%');
+    // calculate the weighted mean score
+    var score = 0;
+    for (i = 0; i < data.length; i++) {
+        score += data[i].score;
+    }
+    score = (score / 4).toPrecision(4);
 
-/*
-  svg.append("svg:text")
-  .attr("class", "aster-score")
-    .attr("dx", "10%")
-    .attr("dy", "-20%")
-    .attr("style", "text-align: right") // text-align: right
-    .text('Pages');
-    */
 
-//});
+    svg.append("svg:text")
+        .attr("class", "aster-score")
+        .attr("dy", ".35em")
+        .attr("text-anchor", "middle") // text-align: right
+        .attr("fill", "#FFF")
+        .text(score + '%');
+
+    /*
+      svg.append("svg:text")
+      .attr("class", "aster-score")
+        .attr("dx", "10%")
+        .attr("dy", "-20%")
+        .attr("style", "text-align: right") // text-align: right
+        .text('Pages');
+        */
+
+    //});
 
 
 }
--- www/js/graph.js
+++ www/js/graph.js
@@ -1,109 +1,165 @@
 function graph(me, friend, genre, section, response) {
 
-if (section=="Pages") 		{ section = "#details1"; };
-if (section=="Checkins") 	{ section = "#details2"; };
-if (section=="Music") 		{ section = "#details3"; };
-if (section=="Movies") 		{ section = "#details4"; };
+    if (section == "Pages") {
+        section = "#details1";
+    };
+    if (section == "Checkins") {
+        section = "#details2";
+    };
+    if (section == "Music") {
+        section = "#details3";
+    };
+    if (section == "Movies") {
+        section = "#details4";
+    };
 
-var graph = {
-  "nodes":[
-    {"name": me,"group":1},
-    {"name": friend,"group":1},
-    {"name": genre,"group":2}
-      
+    var graph = {
+        "nodes": [
+            {
+                "name": me,
+                "group": 1
+            },
+            {
+                "name": friend,
+                "group": 1
+            },
+            {
+                "name": genre,
+                "group": 2
+            }
+
   ],
-  "links":[
-    {"source":me,"target":friend,"value":1},
-   
+        "links": [
+            {
+                "source": me,
+                "target": friend,
+                "value": 1
+            },
+
   ]
-};
-    
-for (i=0; i<response.length; i++) {
-graph.nodes.push({"name": response[i].name,"group":3});
-graph.links.push({"source": me,"target":response[i].name,"group":3});
-graph.links.push({"source": friend,"target":response[i].name,"group":3});
-graph.links.push({"source": genre,"target":response[i].name,"group":3});
-}	
+    };
 
-var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-var height = 300;
+    for (i = 0; i < response.length; i++) {
+        graph.nodes.push({
+            "name": response[i].name,
+            "group": 3
+        });
+        graph.links.push({
+            "source": me,
+            "target": response[i].name,
+            "group": 3
+        });
+        graph.links.push({
+            "source": friend,
+            "target": response[i].name,
+            "group": 3
+        });
+        graph.links.push({
+            "source": genre,
+            "target": response[i].name,
+            "group": 3
+        });
+    }
 
-if (graph.nodes.length>20) { height = 500; }
+    var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+    var height = 300;
 
-    
-var color = d3.scale.category20();
-    
-var force = d3.layout.force()
-     .charge(-2000)
-    .linkDistance(100)
-.chargeDistance(500)
-.gravity(0.5)
-    .size([width, height])
-    .on("tick", tick);
-
-var svg = d3.select(section).append("svg")
-    .attr("width", width)
-    .attr("height", height);
-
-var nodeMap = {};
-
-graph.nodes.forEach(function(d) {
-  nodeMap[d.name] = d;
-});
-
-graph.links.forEach(function(l) {
-  l.source = nodeMap[l.source];
-  l.target = nodeMap[l.target];
-})
-    
-var link = svg.selectAll(".link"),
-    node = svg.selectAll(".node");
-
-  var nodes = graph.nodes,
-      links = graph.links;
-
-  // Restart the force layout.
-  force
-      .nodes(graph.nodes)
-      .links(graph.links) 
-      .start();
-
-  // Update links.
-  var link = svg.selectAll(".link")
-      .data(graph.links)
-    .enter().append("line")
-      .attr("class", "link")
-      .style("stroke-width", function(d) { return Math.sqrt(d.value); });
+    if (graph.nodes.length > 20) {
+        height = 500;
+    }
 
 
-  // Update nodes.
-  node = node.data(nodes, function(d) { return d.name; });
+    var color = d3.scale.category20();
 
-  node.exit().remove();
+    var force = d3.layout.force()
+        .charge(-2000)
+        .linkDistance(100)
+        .chargeDistance(500)
+        .gravity(0.5)
+        .size([width, height])
+        .on("tick", tick);
 
-  var nodeEnter = node.enter().append("g")
-      .attr("class", "node")
-      .call(force.drag);
+    var svg = d3.select(section).append("svg")
+        .attr("width", width)
+        .attr("height", height);
 
-  nodeEnter.append("circle")
-      .attr("r", 13); 
+    var nodeMap = {};
 
-  nodeEnter.append("text")
-      .attr("dy", "-15px")
-      .text(function(d) { return d.name; });
+    graph.nodes.forEach(function (d) {
+        nodeMap[d.name] = d;
+    });
 
-  node.select("circle")
-      .style("fill", function(d) { return color(d.group); });
+    graph.links.forEach(function (l) {
+        l.source = nodeMap[l.source];
+        l.target = nodeMap[l.target];
+    })
+
+    var link = svg.selectAll(".link"),
+        node = svg.selectAll(".node");
+
+    var nodes = graph.nodes,
+        links = graph.links;
+
+    // Restart the force layout.
+    force
+        .nodes(graph.nodes)
+        .links(graph.links)
+        .start();
+
+    // Update links.
+    var link = svg.selectAll(".link")
+        .data(graph.links)
+        .enter().append("line")
+        .attr("class", "link")
+        .style("stroke-width", function (d) {
+            return Math.sqrt(d.value);
+        });
 
 
-function tick() {
-  link.attr("x1", function(d) { return d.source.x; })
-      .attr("y1", function(d) { return d.source.y; })
-      .attr("x2", function(d) { return d.target.x; })
-      .attr("y2", function(d) { return d.target.y; });
+    // Update nodes.
+    node = node.data(nodes, function (d) {
+        return d.name;
+    });
 
-  node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-}
+    node.exit().remove();
+
+    var nodeEnter = node.enter().append("g")
+        .attr("class", "node")
+        .call(force.drag);
+
+    nodeEnter.append("circle")
+        .attr("r", 13);
+
+    nodeEnter.append("text")
+        .attr("dy", "-15px")
+        .text(function (d) {
+            return d.name;
+        });
+
+    node.select("circle")
+        .style("fill", function (d) {
+            return color(d.group);
+        });
+
+
+    function tick() {
+        link.attr("x1", function (d) {
+                return d.source.x;
+            })
+            .attr("y1", function (d) {
+                return d.source.y;
+            })
+            .attr("x2", function (d) {
+                return d.target.x;
+            })
+            .attr("y2", function (d) {
+                return d.target.y;
+            });
+
+        node.attr("transform", function (d) {
+            return "translate(" + d.x + "," + d.y + ")";
+        });
+    }
 
 
 }
